66186cd8cb9b1b75ea6269588ecb7726362271ac
Andre <andrelim865@gmail.com>
2025-11-04 21:02:13 +0700

    feat: change splash screen to stateful

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,23 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      context.go('/input_name');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
