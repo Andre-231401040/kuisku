7eabb786b11f0ee6dfcb4582423faf3b4671bcec
Andre <andrelim865@gmail.com>
2025-11-04 21:03:20 +0700

    feat: add route to name input screen

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kuisku/screens/splash_screen.dart';
+import 'package:kuisku/screens/name_input_screen.dart';
 
 final GoRouter router = GoRouter(
-  routes: <RouteBase>[
+  routes: [
     GoRoute(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
         return const SplashScreen();
       },
-      // routes: <RouteBase>[
-      //   GoRoute(
-      //     path: 'details',
-      //     builder: (BuildContext context, GoRouterState state) {
-      //       return const DetailsScreen();
-      //     },
-      //   ),
-      // ],
+    ),
+    GoRoute(
+      path: '/input_name',
+      builder: (BuildContext context, GoRouterState state) {
+        return const NameInputScreen();
+      },
     ),
   ],
 );
