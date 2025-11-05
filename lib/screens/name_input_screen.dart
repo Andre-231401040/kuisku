fe8b9fcecd30af90561beff612cd8f654932b888
Andre <andrelim865@gmail.com>
2025-11-05 17:56:42 +0700

    fix: add padding to avoid content under transparent appbar

--- lib/screens/name_input_screen.dart
+++ lib/screens/name_input_screen.dart
@@ -24,79 +24,88 @@ class NameInputScreen extends StatelessWidget {
         : screenHeight * 0.04;
 
     return RootScaffold(
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: SizedBox(
-          height: screenHeight,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Masukkan nama Anda',
-                style: TextStyle(
-                  fontFamily: 'Montserrat',
-                  fontSize: heading3,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-
-              SizedBox(height: 28),
-
-              TextField(
-                controller: _textController,
-                style: TextStyle(fontSize: body),
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                    borderSide: BorderSide(
-                      width: 2.0,
-                      color: Color(0xFFD6D6D6),
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                    borderSide: BorderSide(
-                      width: 2.0,
-                      color: Color(0xFF3A86FF),
-                    ),
-                  ),
-                  hintText: 'mis. Andre',
-                  hintStyle: TextStyle(
-                    color: Color(0xFFD6D6D6),
-                    fontSize: body,
+      body: Padding(
+        padding: orientation == Orientation.landscape
+            ? EdgeInsets.only(top: 80)
+            : EdgeInsets.all(0),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: SizedBox(
+            height: screenHeight,
+            child: Column(
+              mainAxisAlignment: orientation == Orientation.landscape
+                  ? MainAxisAlignment.start
+                  : MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Masukkan nama Anda',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: heading3,
+                    fontWeight: FontWeight.w700,
                   ),
                 ),
-              ),
 
-              SizedBox(height: 28),
+                SizedBox(height: 28),
 
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.all(12),
-                    backgroundColor: Color(0xFF3A86FF),
-                    shape: RoundedRectangleBorder(
+                TextField(
+                  controller: _textController,
+                  style: TextStyle(fontSize: body),
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(
+                        width: 2.0,
+                        color: Color(0xFFD6D6D6),
+                      ),
                     ),
-                  ),
-                  onPressed: () {
-                    context.read<UserProvider>().setName(_textController.text);
-                    context.go('/questions');
-                  },
-                  child: Text(
-                    'Mulai Kuis',
-                    style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      color: Colors.white,
-                      fontSize: heading3,
-                      fontWeight: FontWeight.w700,
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(
+                        width: 2.0,
+                        color: Color(0xFF3A86FF),
+                      ),
+                    ),
+                    hintText: 'mis. Andre',
+                    hintStyle: TextStyle(
+                      color: Color(0xFFD6D6D6),
+                      fontSize: body,
                     ),
                   ),
                 ),
-              ),
-            ],
+
+                SizedBox(height: 28),
+
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(12),
+                      backgroundColor: Color(0xFF3A86FF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      ),
+                    ),
+                    onPressed: () {
+                      context.read<UserProvider>().setName(
+                        _textController.text,
+                      );
+                      context.go('/questions');
+                    },
+                    child: Text(
+                      'Mulai Kuis',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        color: Colors.white,
+                        fontSize: heading3,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
